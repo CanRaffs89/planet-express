4b0c7db7dac620a47aa8eb069e712c015c9a70e3
Can <24660563+CanRaffs89@users.noreply.github.com>
2021-09-24 21:06:36 +0100

    Added home link to navbar logo

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/success')
+def succes():
+    return render_template('success.html')
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0')
